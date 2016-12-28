6f227e419197e0ebf2f8b657faef9f75a815cbdb
Aleksandra Kurek <aleksandra.k@maneuver.pl>
2016-12-28 21:06:21 +0100

    prevent scroll on google maps

--- js/app.js
+++ js/app.js
@@ -2,24 +2,24 @@
 
 $(document).ready(function() {
 
-var stickyNavTop = $('.menu-sticky').offset().top;
+    var stickyNavTop = $('.menu-sticky').offset().top;
 
-var stickyNav = function(){
-var scrollTop = $(window).scrollTop();
+    var stickyNav = function() {
+        var scrollTop = $(window).scrollTop();
 
-if (scrollTop > stickyNavTop) {
-    $('.menu-sticky').addClass('sticky');
+        if (scrollTop > stickyNavTop) {
+            $('.menu-sticky').addClass('sticky');
 
-} else {
-    $('.menu-sticky').removeClass('sticky');
-}
-};
+        } else {
+            $('.menu-sticky').removeClass('sticky');
+        }
+    };
 
-stickyNav();
-$(window).scroll(function() {
-  stickyNav();
+    stickyNav();
+    $(window).scroll(function() {
+        stickyNav();
 
-});
+    });
 
 
 });
@@ -30,30 +30,52 @@ $(window).scroll(function() {
 /*responsive tabs*/
 
 $(document).ready(function() {
-  $('#horizontalTab').easyResponsiveTabs({
-    type: 'default', //Types: default, vertical, accordion
-    width: 'auto', //auto or any width like 600px
-    fit: true // 100% fit in a container
-  });
+    $('#horizontalTab').easyResponsiveTabs({
+        type: 'default', //Types: default, vertical, accordion
+        width: 'auto', //auto or any width like 600px
+        fit: true // 100% fit in a container
+    });
 });
 
 /* responsive tabs end*/
 
 /*smooth scrolling */
 
-  $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
 
 /* smooth scrolling end*/
+
+
+/*prevent scroll on google maps*/
+
+$(document).ready(function () {
+    $('#map').addClass('scrolloff');                // set the mouse events to none when doc is ready
+
+    $('#overlay').on("mouseup",function(){          // lock it when mouse up
+        $('#map').addClass('scrolloff');
+        //somehow the mouseup event doesn't get call...
+    });
+    $('#overlay').on("mousedown",function(){        // when mouse down, set the mouse events free
+        $('#map').removeClass('scrolloff');
+    });
+    $("#map").mouseleave(function () {              // becuase the mouse up doesn't work...
+        $('#map').addClass('scrolloff');            // set the pointer events to none when mouse leaves the map area
+                                                    // or you can do it on some other event
+    });
+
+});
+
+/*prevent scroll on google maps end*/
