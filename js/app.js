ca10cb6123adaf55a2972588aa8ed29789b03a0e
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2017-03-14 09:13:52 +0100

    added job info

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-/* sticky menu*/
+/* sticky menu */
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     var stickyNavTop = $('.menu-sticky').offset().top;
 
-    var stickyNav = function() {
+    var stickyNav = function () {
         var scrollTop = $(window).scrollTop();
 
         if (scrollTop > stickyNavTop) {
@@ -16,7 +16,7 @@ $(document).ready(function() {
     };
 
     stickyNav();
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         stickyNav();
 
     });
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
 /*responsive tabs*/
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('#horizontalTab').easyResponsiveTabs({
         type: 'default', //Types: default, vertical, accordion
         width: 'auto', //auto or any width like 600px
@@ -41,8 +41,8 @@ $(document).ready(function() {
 
 /*smooth scrolling */
 
-$(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -63,17 +63,38 @@ $(function() {
 
 $(document).ready(function () {
     $('#map').addClass('scrolloff');
-    $('#overlay').on("mouseup",function(){
+    $('#overlay').on("mouseup", function () {
         $('#map').addClass('scrolloff');
     });
-    $('#overlay').on("mousedown",function(){
+    $('#overlay').on("mousedown", function () {
         $('#map').removeClass('scrolloff');
     });
     $("#map").mouseleave(function () {
         $('#map').addClass('scrolloff');
-    
+
     });
 
 });
 
 /*prevent scroll on google maps end*/
+
+/* to top button */
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("toTop").style.display = "block";
+    } else {
+        document.getElementById("toTop").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+/* end to top button */
